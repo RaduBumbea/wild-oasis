7f05dfd8f6f527c5bb545e44dcfd602723195de7
RaduBumbea <rbumbea-ext@pentalog.com>
2026-01-08 16:54:54 +0200

    add styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
+import Heading from "./ui/Heading.jsx";
+import Row from "./ui/Row.jsx";
+
+const StyledApp = styled.main`
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      <h1>Welcome to Wild Oasis</h1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Welcome to Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button variation="secondary" size="small" onClick={() => alert("Check in")}>Check out</Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
   );
 }
 
